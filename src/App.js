4a76be29c5db0632c7fefb1adf1a31f9f3e448aa
zahirekrem09 <1820980144@stu.adu.edu.tr>
2020-07-14 15:07:13 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/ui/Header";
+import CharacterGrid from "./components/characters/CharacterGrid";
+import axios from 'axios';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() =>{
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+    fetchItems()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header></Header>
+      <CharacterGrid isLoading = {isLoading} items= {items} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const CharacterItem = ({ item }) => {
+  return (
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <img src={item.img} alt="" />
+        </div>
+        <div className="card-back">
+          <h1>{item.name}</h1>
+          <ul>
+            <li>
+              <strong>Actor Name:</strong> {item.portrayed}
+            </li>
+            <li>
+              <strong>Name:</strong> {item.name}
+            </li>
+            <li>
+              <strong>Nickname:</strong> {item.nickname}
+            </li>
+            <li>
+              <strong>Birthday:</strong> {item.birthday}
+            </li>
+            <li>
+              <strong>Status:</strong> {item.status}
+            </li>
+
+            <li>
+              <strong>Occupation:</strong>
+
+              {item.occupation.map((a) => (
+                <li>{a}</li>
+              ))}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterItem;
